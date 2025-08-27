fb2dd223fd0ac9048068751b88491b4d5a5d6032
Emir Kamel <p0092396@brookes.ac.uk>
2025-08-27 18:37:34 +0100

    removing exception for email routing for M100EX forms

--- locallib.php
+++ locallib.php
@@ -774,7 +774,7 @@ function local_obu_forms_update_authoriser($form, $data, $authoriser_id) {
 	$author = get_complete_user_data('id', $data->author);
 	$sc = get_complete_user_data('username', 'csa');
 	$sc_id = $sc->id;
-	if ($form->formref !== "M100EX" && $form->formref !== "M100") { // Use the default CSA Team contact and notification details (PG)
+	if ($form->formref !== "M100") { // Use the default CSA Team contact and notification details (PG)
 		$sc_contact = $sc;
 		$sc_notifications = $sc;
 	} else { // Use the SCAT contact and notification details (UMP)
